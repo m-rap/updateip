bca351247df718a325b30380b7fae8f8042aaae4
rian <mohammad.rian.ap@gmail.com>
2016-04-16 11:45:46 +0700

    add -s param to curl

--- updateip-pastebin.sh
+++ updateip-pastebin.sh
@@ -6,5 +6,5 @@ api_dev_key=       # your pastebin dev key
 api_user_name=     # your pastebin username
 api_user_password= # your pastebin password
 ip=`curl ipecho.net/plain`
-api_user_key=`curl --data "api_dev_key=${api_dev_key}&api_user_name=${api_user_name}&api_user_password=${api_user_password}" ${login_url}`
-curl --data "api_dev_key=${api_dev_key}&api_user_key=${api_user_key}&api_paste_private=1&api_option=paste&api_paste_expire_date=1D&api_paste_name=currip.txt&api_paste_code=${ip}" ${url} ; echo
+api_user_key=`curl -s --data "api_dev_key=${api_dev_key}&api_user_name=${api_user_name}&api_user_password=${api_user_password}" ${login_url}`
+curl -s --data "api_dev_key=${api_dev_key}&api_user_key=${api_user_key}&api_paste_private=1&api_option=paste&api_paste_expire_date=1D&api_paste_name=currip.txt&api_paste_code=${ip}" ${url} ; echo
