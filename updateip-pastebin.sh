067188f4dd2951462abb51ecd79ce6128bf96907
rian <mohammad.rian.ap@gmail.com>
2017-08-05 17:51:14 +0000

    add logging

--- updateip-pastebin.sh
+++ updateip-pastebin.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+log_path="~/.local/share/updateip"
+mkdir -p $log_path
+log_path="$log_path/updateip.log"
 today=`date +%Y%m%d%H%M`
 login_url='http://pastebin.com/api/api_login.php'
 url='http://pastebin.com/api/api_post.php'
@@ -8,4 +11,5 @@ api_user_name=     # your pastebin username
 api_user_password= # your pastebin password
 ip=`curl -s ipecho.net/plain`
 api_user_key=`curl -s --data "api_dev_key=${api_dev_key}&api_user_name=${api_user_name}&api_user_password=${api_user_password}" ${login_url}`
-curl -s --data "api_dev_key=${api_dev_key}&api_user_key=${api_user_key}&api_paste_private=1&api_option=paste&api_paste_expire_date=1D&api_paste_name=currip${today}.txt&api_paste_code=${ip} ${today}" ${url} ; echo
+curl -s --data "api_dev_key=${api_dev_key}&api_user_key=${api_user_key}&api_paste_private=1&api_option=paste&api_paste_expire_date=1D&api_paste_name=currip${today}.txt&api_paste_code=${ip} ${today}" ${url} >> $log_path 2>&1
+echo >> $log_path 2>&1
